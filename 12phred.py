47787b1075fbe4f426c5eb35a992ffaa19102b6f
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-07-02 14:57:20 -0700

    update

--- 12phred.py
+++ 12phred.py
@@ -10,13 +10,18 @@ def char_to_prob(c):
 print('Get the quality and Phred error rate from the following ASCII characters:')
 print('Char: (Q , Prob)')
 print('A   :', char_to_prob('A'))
+# print(char_to_prob(0.001))
 
 # function to get the ASCII character from an error rate
 def prob_to_char(x):
-	q = -10 * math.floor(math.log10(x)) # calculate the quality score, q
-	ascii = chr(q + 33) # get the ASCII character tied to the quality score
-	return q, ascii
+	if type(x) != str: q = -10 * math.log10(x) # calculate the quality score, q
+	else: return None # return None if the probability is a string
+	if q % 1 == 0: # check if quality score is an integer
+		q = math.floor(q) # remove decimal
+		ascii = chr(q + 33) # get the ASCII character tied to the quality score
+		return q, ascii
 
 print('\nGet the quality value and ASCII character from the following error rates:')
 print('Prob : (Q , Char)')
 print('0.001:', prob_to_char(0.001))
+print('A    :', prob_to_char('A'))
