187d1e1935cb27b387ad56c62e495e50445ef402
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-07-02 14:47:04 -0700

    update

--- 12phred.py
+++ 12phred.py
@@ -1,13 +1,9 @@
 # 12phred.py by Ricky Cantua
 import math
 
-# function to turn an ASCII value to quality score
-def ascii_to_qual(n):
-	if n >= 33 and n <= 75: return n - 33
-
 # function to get quality and error rate from an ASCII character
 def char_to_prob(c):
-	q = ascii_to_qual(ord(c)) # calculate the quality score from ASCII value, ord(c)
+	q = ord(c) - 33 # calculate the quality score from ASCII value, ord(c)
 	p = 10 ** -(q/10) # calculate the error rate, p
 	return q, p
 
@@ -18,7 +14,8 @@ print('A   :', char_to_prob('A'))
 # function to get the ASCII character from an error rate
 def prob_to_char(x):
 	q = -10 * math.floor(math.log10(x)) # calculate the quality score, q
-	return q, chr(q)
+	ascii = chr(q + 33) # get the ASCII character tied to the quality score
+	return q, ascii
 
 print('\nGet the quality value and ASCII character from the following error rates:')
 print('Prob : (Q , Char)')
