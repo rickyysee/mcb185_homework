e425431cafc82d39cef43bc8a81716c795efe411
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-07-09 16:12:51 -0700

    update

---
+++ iterpractice.py
@@ -0,0 +1,12 @@
+# iterpractice.py by Ricky Cantua
+
+# function that calculates the triangular number (sum of numbers from 1 to n)
+def triangular_num(n):
+	current = 0 # initialize a variable to store the sum
+	for i in range(1, n+1): # end at specified number, end range is n+1
+		current = current + i # store the sum in current variable
+		# print(i, current)
+	return(current)
+print('Triangular number of 5 is:', triangular_num(5))
+
+# function that calculates factorial of a number
