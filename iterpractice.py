91a5b8b345afe0485f29d26716393b1e2049bda7
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-07-10 12:48:27 -0700

    update

--- iterpractice.py
+++ iterpractice.py
@@ -28,7 +28,16 @@ The probability of getting exactly 9 (k) customers in one hour is a Poisson calc
 def poisson(n, k): return (n**k) * (math.e**-n) / factorial(k) # previous factorial() fxn
 print('If expected is 6, probability of 9 is:', poisson(6, 9))
 
-# function that solves 'n choose k': n! / k!(n-k)!
+# solve for 'n choose k': n! / k!(n-k)!
 def combination(n, k): return factorial(n) / (factorial(k) * factorial(n-k))
 print('5 choose 3 is:', combination(5, 3))
 print('5 choose 5 is:', combination(5, 5))
+
+# estimate Euler's number (e = 2.71828...)
+# infinite sum of 1/n!
+# choose finite number of iterations
+def euler_est(n):
+	current = 0
+	for i in range(n): current = current + 1/factorial(i)
+	return current
+print('Estimating Euler\'s number with 10 iterations is:', euler_est(10))
