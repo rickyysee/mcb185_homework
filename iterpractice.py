2fa3c09f4aeaff57f2381c87e9847920ffd0fab4
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-07-17 13:06:40 -0700

    update

--- iterpractice.py
+++ iterpractice.py
@@ -15,9 +15,9 @@ def factorial(n):
 	current = 1 # initialize variable to store product
 	for i in range(1, n+1):
 		current = current * i # store the product of current variable
-		# print(i, current)
 	return current
 print('Factorial of 5 is:', factorial(5))
+print('Factorial of 0 is:', factorial(0))
 
 # Poisson probability of k events occurring with an expectation of n:
 # n^k e^-n / k!
@@ -25,7 +25,8 @@ print('Factorial of 5 is:', factorial(5))
 A coffee shop receives an average of 6 (n) customers every hour.
 The probability of getting exactly 9 (k) customers in one hour is a Poisson calculation.
 """
-def poisson(n, k): return (n**k) * (math.e**-n) / factorial(k) # previous factorial() fxn
+def poisson(n, k): 
+	return (n**k) * (math.e**-n) / factorial(k) # previous factorial() fxn
 print('If expected is 6, probability of 9 is:', poisson(6, 9))
 
 # solve for 'n choose k': n! / k!(n-k)!
