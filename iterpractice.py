7440b73f7925a2d8cc3df2b911a97e6f5fcd6ff2
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-07-20 16:23:50 -0700

    update

--- iterpractice.py
+++ iterpractice.py
@@ -8,6 +8,7 @@ def triangular_num(n):
 		current = current + i # store the sum in current variable
 		# print(i, current)
 	return current
+# test code
 print('Triangular number of 5 is:', triangular_num(5))
 
 # function that calculates factorial of a number
@@ -16,21 +17,20 @@ def factorial(n):
 	for i in range(1, n+1):
 		current = current * i # store the product of current variable
 	return current
+# test code
 print('Factorial of 5 is:', factorial(5))
 print('Factorial of 0 is:', factorial(0))
 
 # Poisson probability of k events occurring with an expectation of n:
 # n^k e^-n / k!
-"""
-A coffee shop receives an average of 6 (n) customers every hour.
-The probability of getting exactly 9 (k) customers in one hour is a Poisson calculation.
-"""
 def poisson(n, k): 
 	return (n**k) * (math.e**-n) / factorial(k) # previous factorial() fxn
+# test code
 print('If expected is 6, probability of 9 is:', poisson(6, 9))
 
 # solve for 'n choose k': n! / k!(n-k)!
 def combination(n, k): return factorial(n) / (factorial(k) * factorial(n-k))
+# test code
 print('5 choose 3 is:', combination(5, 3))
 print('5 choose 5 is:', combination(5, 5))
 
@@ -41,6 +41,7 @@ def euler(n):
 	current = 0
 	for i in range(n): current = current + 1/factorial(i)
 	return current
+# test code
 print('Estimating Euler\'s number with 10 iterations is:', euler(10))
 
 # determine if a number is prime
@@ -53,7 +54,7 @@ def is_prime(n):
 		if n % i == 0:   factors = factors + 1 # count number of factors to n-1
 		if factors > 1:  return False # if more than 1 factors (1 is known)
 	return True # if not enough factors found, number is prime
-
+# test code
 print('Prime numbers:')
 for i in range(0,11): print(i, is_prime(i)) # for loop of tests
 print('1035', is_prime(1035))
@@ -62,7 +63,10 @@ print('9677', is_prime(9677))
 # estimate pi (3.14159...) using Nilakantha series
 def pi_est(n):
 	nilak = 3
-	for i in range(0, n+1): 
+	for i in range(0, n): 
 		nilak = nilak + (4 * (-1)**i) / (factorial(4 + 2*i) / factorial(1 + 2*i))
 	return nilak
-print('Pi estimated with 10 iterations:', pi_est(10))
+# test code
+print('Pi estimated up to 10 iterations:')
+for i in range(1, 11):
+	print(pi_est(i))
