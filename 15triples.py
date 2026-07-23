798102413aebf5bc6455b6f5855cf3f5733b2358
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-07-23 16:37:05 -0700

    triples-update

--- 15triples.py
+++ 15triples.py
@@ -5,9 +5,10 @@ import math
 # find all Pythagorean triples for triangles with sides a and b less than 100
 # i.e. 3, 4, and 5 are Pythagorean triples (3^2 + 4^2 = 5^2)
 
-for i in range(1, 99):
-	a = i
-	b = i + 1
-	c = math.sqrt(a**2 + b**2)
-	if c % 1 == 0:
-		print(a, b, c)
+for i in range(1, 100):
+	a = i # check every a from 1-99
+	for j in range(i+1, 100): # check values of b after a through 99
+		b = j
+		c = math.sqrt(a**2 + b**2) # calculate c
+		if c % 1 == 0: # check if hypotenuse is an integer
+			print(a, b, math.floor(c))
