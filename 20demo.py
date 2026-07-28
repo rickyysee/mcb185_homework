73084cf622020823e959f61b3c0837119ea58666
ricky <156491520+rickyysee@users.noreply.github.com>
2026-07-27 22:35:31 -0700

    update

---
+++ 20demo.py
@@ -0,0 +1,33 @@
+# 20demo.py by Ricky Cantua
+
+import math
+
+# strings are wrapped in quotes
+s = 'hello world'
+print(s)
+# mix quote types to print them
+s1 = 'hey "dude"'
+s2 = "don't tell me what to do"
+print(s1, s2)
+# use backslashes to escape quotes
+print('hey "dude" don\'t tell me what to do')
+
+# string methods
+print(s.upper())
+print(s)
+print(s.replace('o', ''))
+print(s.replace('o', '').replace('r', 'i'))
+
+# string formatting
+print('\nString Formatting')
+# f-strings
+print(f'{math.pi}')            # does nothing really
+print(f'{math.pi:.3f}')        # 3 fixed digits after decimal
+print(f'{1e6 * math.pi:e}')    # exponent notation
+print(f'{"hello world":>20}')  # right justify with space filler
+print(f'{"hello world":.>20}') # right justify with dot filler
+print(f'{20:.<9}{10}')        # left justify
+# str.format()
+print('{} {:.3f}'.format('str.format', math.pi))
+# printf-style
+print('%s %.3f' % ('printf', math.pi))
