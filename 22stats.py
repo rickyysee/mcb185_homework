1df06b8b8fca9e77c7481cf84d7105bb3e53b9f4
ricky <156491520+rickyysee@users.noreply.github.com>
2026-08-22 21:01:37 -0700

    wip

--- 22stats.py
+++ 22stats.py
@@ -43,16 +43,43 @@ def sd(list):
 	return total ** (1/2)
 
 # median without sorting (use the median of medians method)
-def median(list):
+def median_medians(list):
 	n = len(list)
+	mid = n//2
+	if n % 2 == 0: mid2 = n//2 - 1
+	# use sort if the list is small
 	if n <= 10:
 		list.sort()
-		if n % 2 == 1: return list[n//2]
-		else:          return (list[n//2] + list[n//2 - 1]) / 2
-	for i in range(0, 5):
-		print(list[i])
+		if n % 2 == 1: return list[mid]
+		else:          return (list[mid] + list[mid2]) / 2
+	
+	# adapted from brilliant.org
+	# make lists of 5 or less, then find their medians
+	sublists = [list[j:j+5] for j in range(0, len(list), 5)]
+	medians = [sorted(sublist)[len(sublist)//2] for sublist in sublists]
+
+	print(sublists)
+	print(medians)
+
+	# choose a good pivot point
+	if len(medians) <= 5: pivot = sorted(medians)[len(medians)//2]
+	else:                 pivot = median_medians(medians, len(medians)//2)
+
+	print(pivot)
+
+	# partition list to values smaller/bigger than pivot
+	lo = [j for j in list if j < pivot]
+	hi = [j for j in list if j > pivot]
+
+	print(lo)
+	print(hi)
+
+	k = len(lo)
+	if mid < k:   return median_medians(lo)
+	elif mid > k: return median_medians(hi)
+	else:         return pivot
 
 print('number of values:', len(nums))
 print('min and max:', min(nums), max(nums))
 print('mean and sd:', mean(nums), sd(nums))
-print('median:', median(nums))
+print('median:', median_medians(nums))
