4f9e98565abe6e547a1869108eab00aea5e79195
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-08-25 15:59:19 -0700

    count any letters

--- 33ntcomp.py
+++ 33ntcomp.py
@@ -4,15 +4,15 @@ import mcb185
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
 	defwords = defline.split()
 	name = defwords[0]
-	A = 0
-	C = 0
-	G = 0
-	T = 0
-	N = 0
+	nts = []
+	counts = []
 	for nt in seq:
-		if   nt == 'A': A += 1
-		elif nt == 'C': C += 1
-		elif nt == 'G': G += 1
-		elif nt == 'T': T += 1
-		else:           N += 1
-	print(name, A/len(seq), C/len(seq), G/len(seq), T/len(seq), N/len(seq))
+		if nt not in nts:
+			nts.append(nt)
+			counts.append(0)
+		idx = nts.index(nt)
+		counts[idx] += 1
+	print(name)
+	for nt, n in zip(nts, counts):
+		print(nt, n, n/len(seq))
+	print()
