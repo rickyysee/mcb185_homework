5dc571c4d951c9fb41d3dd498fa2938ee542dca9
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-08-27 15:25:45 -0700

    add kyte-doolittle function and codes

---
+++ 35transmembrane.py
@@ -0,0 +1,68 @@
+import sys
+import mcb185
+import sequence
+
+# handle arguments
+args = sys.argv
+if len(args) != 2:
+	sys.exit('Error: unexpected number of arguments')
+file = args[1]
+
+# get deflines and sequences
+seqs = mcb185.read_fasta(file)
+
+# transmembrane proteins
+# hydrophobic N-terminal signal peptide
+	# 8 aa long segment with average KD >= 2.5 in first 30 aas
+# transmembrane region(s)
+	# 11 aa long segment with average KD >= 2.0 after first 30 aas
+# neither signal or transmembrane should have Proline (P)
+
+# find hydrophobic signal
+for defline, seq in seqs:
+	sig_flag = False
+	txm_flag = False
+
+	# check first 8 aminos
+	nterm = seq[0:30]
+	window = nterm[0:8]
+	kd = sequence.kyte_doolittle(window)
+	if kd / 8 >= 2.5 and 'P' not in window: 
+		sig_flag = True
+	
+	# check every other window of 8 aminos
+	if sig_flag == False:
+		for i in range(1, len(nterm) -8 +1):
+			window = nterm[i:i+8]
+			lose = nterm[i-1]
+			gain = nterm[i+8-1]
+			if lose in sequence.AMINOKD:
+				kd -= sequence.AMINOKD[lose]
+			if gain in sequence.AMINOKD:
+				kd += sequence.AMINOKD[gain]
+			if kd / 8 >= 2.5 and 'P' not in window:
+				sig_flag = True
+
+	if sig_flag == False: continue
+
+	# check first window
+	postnt = seq[31:len(seq)]
+	window = postnt[0:11]
+	kd = sequence.kyte_doolittle(window)
+	if kd / 11 >= 2.0 and 'P' not in window: 
+		txm_flag = True
+	
+	# check every other window of 11 aminos
+	if txm_flag == False:
+		for i in range(1, len(nterm) -11 +1):
+			window = nterm[i:i+11]
+			lose = nterm[i-1]
+			gain = nterm[i+11-1]
+			if lose in sequence.AMINOKD:
+				kd -= sequence.AMINOKD[lose]
+			if gain in sequence.AMINOKD:
+				kd += sequence.AMINOKD[gain]
+			if kd / 11 >= 2.0 and 'P' not in window:
+				txm_flag = True
+
+	if txm_flag == True: print(defline)
--- sequence.py
+++ sequence.py
@@ -25,6 +25,22 @@ def gc_skew(seq):
 	if c + g == 0: return 0
 	return (g - c) / (g + c)
 
+
+
+AMINOKD = {
+	'I' :  4.5,	'V' :  4.2,	'L' :  3.8, 'F' :  2.8, 'C' :  2.5,
+	'M' :  1.9,	'A' :  1.8,	'G' : -0.4, 'T' : -0.7, 'S' : -0.8,
+	'W' : -0.9, 'Y' : -1.3, 'P' : -1.6, 'H' : -3.2, 'E' : -3.5,
+	'Q' : -3.5, 'D' : -3.5, 'N' : -3.5, 'K' : -3.9, 'R' : -4.5
+}
+
+# calculate total hydrophobicity using kyle-dolittle scores
+def kyte_doolittle(seq):
+	kd = 0
+	for aa in seq:
+		if aa in AMINOKD: kd += AMINOKD[aa]
+	return kd
+
 # translate a dna sequence to amino acid sequence
 def translate(dna):
 	codons = ('ATG', 'TAA', 'TAG', 'TGA')
