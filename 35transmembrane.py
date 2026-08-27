026752d0677e8ba197116c2cbdc6174415ee2b5d
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-08-27 15:49:09 -0700

    change txm_flag to check postnt

--- 35transmembrane.py
+++ 35transmembrane.py
@@ -23,46 +23,44 @@ for defline, seq in seqs:
 	sig_flag = False
 	txm_flag = False
 
+	## signal check
 	# check first 8 aminos
 	nterm = seq[0:30]
 	window = nterm[0:8]
 	kd = sequence.kyte_doolittle(window)
-	if kd / 8 >= 2.5 and 'P' not in window: 
-		sig_flag = True
+	if kd / 8 >= 2.5 and 'P' not in window: sig_flag = True
 	
 	# check every other window of 8 aminos
 	if sig_flag == False:
 		for i in range(1, len(nterm) -8 +1):
 			window = nterm[i:i+8]
-			lose = nterm[i-1]
-			gain = nterm[i+8-1]
-			if lose in sequence.AMINOKD:
-				kd -= sequence.AMINOKD[lose]
-			if gain in sequence.AMINOKD:
-				kd += sequence.AMINOKD[gain]
+			lose   = nterm[i-1]
+			gain   = nterm[i+8-1]
+			if lose in sequence.AMINOKD: kd -= sequence.AMINOKD[lose]
+			if gain in sequence.AMINOKD: kd += sequence.AMINOKD[gain]
 			if kd / 8 >= 2.5 and 'P' not in window:
 				sig_flag = True
+				break
 
 	if sig_flag == False: continue
 
+	## transmembrane region check
 	# check first window
 	postnt = seq[31:len(seq)]
 	window = postnt[0:11]
 	kd = sequence.kyte_doolittle(window)
-	if kd / 11 >= 2.0 and 'P' not in window: 
-		txm_flag = True
+	if kd / 11 >= 2.0 and 'P' not in window: txm_flag = True
 	
 	# check every other window of 11 aminos
 	if txm_flag == False:
-		for i in range(1, len(nterm) -11 +1):
-			window = nterm[i:i+11]
-			lose = nterm[i-1]
-			gain = nterm[i+11-1]
-			if lose in sequence.AMINOKD:
-				kd -= sequence.AMINOKD[lose]
-			if gain in sequence.AMINOKD:
-				kd += sequence.AMINOKD[gain]
+		for i in range(1, len(postnt) -11 +1):
+			window = postnt[i:i+11]
+			lose   = postnt[i-1]
+			gain   = postnt[i+11-1]
+			if lose in sequence.AMINOKD: kd -= sequence.AMINOKD[lose]
+			if gain in sequence.AMINOKD: kd += sequence.AMINOKD[gain]
 			if kd / 11 >= 2.0 and 'P' not in window:
 				txm_flag = True
+				break
 
-	if txm_flag == True: print(defline)
+	if txm_flag == True: print(f'{defline:.50}')
