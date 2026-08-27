2a107e5f3cca01415ac304b1c16c6ede7727fef3
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-08-27 12:00:50 -0700

    remove slow script and fix comp calc

--- 34skew.py
+++ 34skew.py
@@ -15,28 +15,33 @@ if len(args) == 3: w = int(args[2])
 seqs = []
 with gzip.open(file, 'rt') as fp:
 	for line in fp:
-		if line[0] == '#' or line[0] == '\n': continue
+		l = line[0]
+		if l == '>' or l == '\n' or l == '#': continue
 		line = line.strip()
 		seqs.append(line)
 	seq = ''.join(seqs)
+
+# handle sequences smaller than window
+seq_length = len(seq)
+if w > seq_length: w = seq_length
 
 # count GCs in first window
 firstw = seq[0:w]
 g = firstw.count('G')
 c = firstw.count('C')
-comp = g + c / w
+comp = (g + c) / w
 if g + c == 0: skew = 0
 else         : skew = (g - c) / (g + c) 
 print(0, comp, skew)
 
 # get gc-skew by checking preceeding and final base of each window
-for i in range(1, len(seq) -w +1):
+for i in range(1, seq_length -w +1):
 	s = seq[i-1] + seq[i+w-1]
 	if s[0] == 'G': g -= 1
 	if s[0] == 'C': c -= 1
 	if s[1] == 'G': g += 1
 	if s[1] == 'C': c += 1
-	comp = g + c / w
+	comp = (g + c) / w
 	if g + c == 0: skew = 0
 	else         : skew = (g - c) / (g + c)
 	print(i, comp, skew) 
